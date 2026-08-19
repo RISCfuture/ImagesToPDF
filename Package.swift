3114d9d3b295217ce200c12bcdbfa2ad1b885fbc
Tim Morgan <github@timothymorgan.info>
2026-08-18 19:25:48 -0700

    Fail the build on DocC warnings

--- Package.swift
+++ Package.swift
@@ -13,7 +13,8 @@ let package = Package(
   platforms: [.macOS(.v13)],
   dependencies: [
     .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
-    .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0")
+    .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
+    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.5.0")
   ],
   targets: [
     .executableTarget(
